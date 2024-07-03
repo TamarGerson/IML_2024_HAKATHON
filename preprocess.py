8b4afb49e3cc2e2d1891f18b2a1cb8659e70bc1f
kiralon <alonkirel@gmail.com>
2024-07-03 20:55:41 +0300

    TODO

---
+++ preprocess.py
@@ -0,0 +1,28 @@
+
+
+#TODO: trip_id -> int (id)
+#TODO: part -> int (id)
+#TODO: trip_id_unique_station -> int (id)
+#TODO: trip_id_unique -> int (id)
+#TODO: line_id id -> int (id)
+# IS LINE ID RELEVANT (DEST 9 vs 15 rehavia)
+#TODO: alternative -> number of alternatives?
+#TODO: cluster -> int(id)
+#TODO: station_name -> needed? id and index
+#TODO: arrival_time -> hh only? peak time only {0,1}
+# DO WE NEED TO ADD time^2
+#TODO: door_closing_time - arrival_time ? more passenj.
+#TODO: passengers_continue -> threshold? pas_up?
+
+
+
+#TODO ALL: 
+#   -> clean null
+#   -> 
+
+#TODO PASSENGERS:
+#   -> no floats (clean_half_pepole())
+
+
+#TODO station :
+#   -> door_closing_time - arrival_time < 0 ? 
