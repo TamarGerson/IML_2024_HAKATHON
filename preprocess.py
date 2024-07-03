541ae1d8690ae90fb0e1dc8c2f78192a0e010bf1
kiralon <alonkirel@gmail.com>
2024-07-03 21:03:35 +0300

    delete_null_and_outliers()

--- preprocess.py
+++ preprocess.py
@@ -18,7 +18,27 @@
 
 #TODO ALL: 
 #   -> clean null
+def delete_null_and_outliers(X, y = None):
+    
+    # X = delete_outliers(X) #WHAT ARE THE OUTLIERS?
+    df = X.copy()
+    df["lable"] = y
+    
+    df = df.dropna() #.drop_duplicates() 
+    df = df[df["lable"] < 0] # no negative number of pass
+    
+    y = df["lable"]
+    X = df.drop(columns = ["lable"])
+    
+    return X, y
+
+
+def delete_outliers(X):
+    #WHAT ARE THE OUTLIERS?
+    return X
 #   -> 
+
+
 
 #TODO PASSENGERS:
 #   -> no floats (clean_half_pepole())
