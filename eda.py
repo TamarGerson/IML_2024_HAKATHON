3b54a7db9fd38087a674b42c5e81a601dedb5af7
ofirhol1 <ofir.holtzman@mail.huji.ac.il>
2024-07-04 10:26:29 +0300

    eda adding the saving fig function

--- eda.py
+++ eda.py
@@ -1,11 +1,30 @@
 import pandas as pd
-def plot_correlation(df, column1, column2):
-    # Calculate the correlation coefficient
-    correlation, _ = pearsonr(df[column1].dropna(), df[column2].dropna())
+import plotly.express as px
+import os
+from scipy.stats import pearsonr
+
+def save_plotly_fig(fig, folder, file_name):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
     
-    # Create the plot with the correlation coefficient in the title
+    file_path = os.path.join(folder, file_name)
+    fig.write_html(file_path)
+
+def plot_correlation(df, column1, column2):
+    correlation, _ = pearsonr(df[column1], df[column2])
     title = f'Correlation Plot between {column1} and {column2} (r = {correlation:.2f})'
     fig = px.scatter(df, x=column1, y=column2, trendline="ols",
                      title=title, labels={column1: column1, column2: column2})
-    fig.show()
+    return fig
+
+def plot_all_correlations(df, target_column, save_folder):
+    for column in df.columns:
+        if column != target_column and pd.api.types.is_numeric_dtype(df[column]):
+            fig = plot_correlation(df, column, target_column)
+            file_name = f'correlation_{column}_vs_{target_column}.html'
+            save_plotly_fig(fig, save_folder, file_name)
+
+
+
+
 
